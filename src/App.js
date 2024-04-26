fbb80c105c60ce7a6934253c86559294a750c04d
tonytank <avillanueva@arkusnexus.com>
2024-04-25 19:31:32 -0700

    pipeline test3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is my first react app. Pipeline 2</h1>
+      <h1>This is my first react app. Pipeline test 3</h1>
       <h1>Hello</h1>
       <h1>Azure DevOps learning path.</h1>
       <header className="App-header">
